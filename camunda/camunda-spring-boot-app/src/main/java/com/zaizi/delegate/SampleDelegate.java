ebe9d611f1b0fb57a49abe732f7018c19b2d072f
Giuseppe Malanga <gmalanga@zaizi.com>
2021-05-28 11:49:25 +0100

    Added a new bpmn file with the form

---
+++ camunda/camunda-spring-boot-app/src/main/java/com/zaizi/delegate/SampleDelegate.java
@@ -0,0 +1,19 @@
+package com.zaizi.delegate;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SampleDelegate implements JavaDelegate {
+	
+	private final Logger LOGGER = LoggerFactory.getLogger(SampleDelegate.class);
+	
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		
+		LOGGER.info("Sample delegate ");
+		
+	}
+
+}
